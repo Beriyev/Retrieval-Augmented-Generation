e8fa03d9bfb2f9d194340af66ad89de318c31810
Parthiv B <itsamevirus@gmail.com>
2026-06-26 22:09:26 +0530

    added semantic chunker

--- app/main.py
+++
@@ -1,8 +0,0 @@
-from agent import extractor, chunker
-
-pages = extractor("C:/RAG Project/docs/Unit_1.pdf")
-chunks = chunker(pages, filepath="C:/RAG Project/docs/Unit_1.pdf")
-
-print(f"Total chunks: {len(chunks)}")
-for c in chunks[:5]:  # just look at the first 5
-    print(c)
